aded5b86def4bd514f2efa1948449e177784af5e
José Frazão <102549534+josef10000@users.noreply.github.com>
2023-04-04 14:43:28 -0300

    Exercício 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+
+            double num1, num2, media;
+
+            Console.Write("Digite o primeiro número: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            media = (num1 + num2) / 2;
+
+            Console.WriteLine("A média aritmética entre {0} e {1} é: {2}", num1, num2, media);
+
+            Console.ReadKey();
+        }
+    }
+}
