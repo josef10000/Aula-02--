d29472016156a7883ce711ce43c0dc66551e438a
José Frazão <102549534+josef10000@users.noreply.github.com>
2023-04-04 14:50:40 -0300

    Exercicio 2

---
+++ Exercicio 2.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10, B = 20, temp;
+
+            Console.WriteLine("Antes da troca: A = {0}, B = {1}", A, B);
+
+            temp = A;
+            A = B;
+            B = temp;
+
+            Console.WriteLine("Depois da troca: A = {0}, B = {1}", A, B);
+
+            Console.ReadKey();
+        }
+    }
+}
